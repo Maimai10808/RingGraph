2d1f59843698058f76b7df2e769656d285b945de
Maimai10808 <331686549@qq.com>
2025-07-14 23:31:12 +0800

    RingGraph & Pic

--- RingGraph/ViewController.swift
+++ RingGraph/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ringGraph: RingGraphView!
+    
+    @IBOutlet weak var slider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        ringGraph.progress = CGFloat(slider.value)
     }
 
-
+    @IBAction func sliderDidChange(_ sender: UISlider) {
+        ringGraph.progress = CGFloat(sender.value)
+    }
+    
 }
 
